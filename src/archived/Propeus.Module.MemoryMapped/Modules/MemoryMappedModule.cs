62df7267cb2bc8a6f9c307ec4822e48fb196be37
Propeus <hillgner_forza@hotmail.com>
2023-09-24 17:27:53 -0300

    Move projetos para a pasta archived

---
+++ src/archived/Propeus.Module.MemoryMapped/Modules/MemoryMappedModule.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Propeus.Module.Abstract;
+using Propeus.Module.Abstract.Attributes;
+using Propeus.Module.Abstract.Interfaces;
+
+namespace Propeus.Modulo.MemoryMapped.Modules
+{
+    /// <summary>
+    /// Modulo para compartilhar outros modulos entre processos
+    /// </summary>
+    [Module(AutoStartable = false, AutoUpdate = false, Description = "Modulo para compartilhar outros modulos entre processos", KeepAlive = false, Singleton = true)]
+    public class MemoryMappedModule : BaseModule
+    {
+        private MemoryMappedFile _sharedIndex;
+        private MemoryMappedViewAccessor _acc;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public MemoryMappedModule(IModuleManager moduleManager)
+        {
+            //Criar um sistema de compatilhamento de modulo por memoria
+        }
+
+        public override void ConfigureModule()
+        {
+            _sharedIndex = MemoryMappedFile.CreateOrOpen("module_shared_index", 1024, MemoryMappedFileAccess.ReadWriteExecute);
+            Stream stream = _sharedIndex.CreateViewStream(0,1024);
+            // Cria um ponteiro para a estrutura MyObject
+            IntPtr pointer = IntPtr.Add((int)stream.Position, sizeof(long));
+            // Cria uma instância do objeto .NET na memória mapeada
+            object instance = Marshal.PtrToStructure(pointer, typeof(MemoryMappedModule));
+
+            base.ConfigureModule();
+        }
+
+        public override void Launch()
+        {
+            base.Launch();
+        }
+    }
+}
---
+++ src/archived/Propeus.Modulo.Console/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Propeus.Module.Abstract;
+using Propeus.Module.Abstract.Attributes;
+using Propeus.Module.Abstract.Interfaces;
+using Propeus.Modulo.Console.Contracts;
+
+namespace Propeus.Modulo.Console
+{
+
+    //TODO: Caso o atributo possua AutoStartable, procurar o metodo Launch e anexar ao taskjob se estiver disponivel,
+    //caso contrario inicar o launch de void e o usuario deve lidar com o ciclo de vida do modulo
+    /// <summary>
+    /// Exemplo de modulo auto inicializavel e funcional
+    /// </summary>
+    [Module(AutoStartable = true, Singleton = true)]
+    public class Program : BaseModule
+    {
+        //Converter o Modulo.CLI em exe e dar um jeito de executar ele mesmo dentro de um gerenciador
+        //#estouficandoloucosopode
+
+        private readonly IModuleManager _moduleManager;
+
+        private readonly CancellationTokenSource _cts;
+        private ProcessStartInfo _processConsoleStartInfo;
+        private Process _processConsole;
+        private StreamWriter _inputConsoleProcess;
+        private StreamReader _outputConsoleProcess;
+        private Task _taskConsoleInputUser;
+
+        /// <summary>
+        /// Construtor padrao do <see cref="Program"/>
+        /// </summary>
+        /// <param name="moduleManager"></param>
+        public Program(IModuleManager moduleManager) : base()
+        {
+            _moduleManager = moduleManager;
+            _cts = new CancellationTokenSource();
+        }
+
+        static void Main(string[] args)
+        {
+
+            //using (IModuleManager moduleManager = Propeus.Module.Manager.ModuleManagerExtensions.CreateModuleManager())
+            //{
+
+            //    AppDomain.CurrentDomain.
+            //}
+
+        }
+
+        ///<inheritdoc/>
+        public override void ConfigureModule()
+        {
+            this._processConsoleStartInfo = new ProcessStartInfo();
+            this._processConsoleStartInfo.WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            this._processConsoleStartInfo.UseShellExecute = false;
+            this._processConsoleStartInfo.RedirectStandardInput = false;
+            this._processConsoleStartInfo.RedirectStandardOutput = false;
+            this._processConsoleStartInfo.CreateNoWindow = true;
+            this._processConsoleStartInfo.FileName = "Module.exe";
+
+            this._processConsole = new Process();
+            this._processConsole.StartInfo = this._processConsoleStartInfo;
+
+
+            base.ConfigureModule();
+        }
+
+        ///<inheritdoc/>
+        public override void Launch()
+        {
+            this._processConsole.Start();
+            //this._inputConsoleProcess = this._processConsole.StandardInput;
+            //this._outputConsoleProcess = this._processConsole.StandardOutput;
+            this._taskConsoleInputUser = Task.Run(CaptureInputUser, _cts.Token);
+            base.Launch();
+        }
+
+        private void CaptureInputUser()
+        {
+            while (!_cts.IsCancellationRequested)
+            {
+                if (_outputConsoleProcess != null)
+                {
+                    var sb = new StringBuilder();
+                    while (!_outputConsoleProcess.EndOfStream)
+                    {
+                        sb.Append((char)_outputConsoleProcess.Read());
+                    }
+
+                    //moduloCliContrato.ExecutarCLI(sb.ToString().Split(' '));
+                    sb.Clear();
+                    Task.Delay(TimeSpan.FromSeconds(1));
+                }
+                else
+                {
+                    var input = System.Console.ReadLine();
+                    //moduloCliContrato.ExecutarCLI(input.Split(' '));
+                }
+
+            }
+        }
+    }
+
+
+}
